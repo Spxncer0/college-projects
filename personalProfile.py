b29b10bb6ea83772e3f19b90ac6ff6b4617836e6
Spxncer0 <111756167+Spxncer0@users.noreply.github.com>
2023-10-14 16:27:45 +0100

    Add personalProfile

---
+++ personalProfile.py
@@ -0,0 +1,9 @@
+name = input('What is your name?: ')
+age = input('How old are you?: ')
+favourite_colour = input('What is your favourite colour?: ')
+# These are my variables
+
+print('Name: ' + name)
+print('Age: ' + age)
+print('Favourite Colour: ' + favourite_colour)
+# This prints out the variables
