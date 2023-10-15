29bd8888716935056a8f0232feee45a46728fe18
Spxncer0 <111756167+Spxncer0@users.noreply.github.com>
2023-10-15 12:01:33 +0100

    Add degrees symbols and catch user input error

--- tempConverter.py
+++ tempConverter.py
@@ -1,14 +1,25 @@
-# Setting variables, with full caps = constant
-temperature = int(input('Enter a temperature: '))
+# Catching user error
+while True:
+  try:
+    temperature = float(input('Enter a temperature: '))
+    break
+  except ValueError:
+    print('Please enter a number')
+
 CELSIUS = temperature * 1.8 + 32
-FARENHEIT = ((temperature - 32) / 1.8)
+FARENHEIT = (temperature - 32) / 1.8
 
 # Asking the user what metric the degrees is in
-metric = input('In celsius or farenheit? (c/f): ')
+while True:
+  metric = input('In celsius or farenheit? (c/f): ')
+  if metric == 'c' or metric == 'f':
+    break
+  else:
+    print('Please enter either celsius or farenheit (c/f)')
 
 # Printing the conversion depending on user input
 if metric == 'c':
-  print(CELSIUS)
+  print(f'It is {CELSIUS}° farenheit!')
 
-elif metric == "f":
-  print(FARENHEIT)
+elif metric == 'f':
+  print(f'It is {FARENHEIT}° celsius!')
