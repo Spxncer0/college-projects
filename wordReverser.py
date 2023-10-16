cc75a44ab4be94c59b2ae0aad7fb5febc52d2d03
Spxncer0 <111756167+Spxncer0@users.noreply.github.com>
2023-10-16 16:15:43 +0100

    Add wordReverser

---
+++ wordReverser.py
@@ -0,0 +1,15 @@
+# Started 16/10/2023
+
+while True:
+    word = input('Choose a word or a number, and it will be reversed: ')
+    reversed_word = word[::-1]
+    print(reversed_word)
+
+    again = input('Would you like to input a new word or number? (y/n): ')
+    while again != 'y' and again != 'n':
+        print('Please enter either y or n')
+        again = input('Would you like to input a new word or number? (y/n): ')
+    
+    if again == 'n':
+        print('Have a good day!')
+        break
