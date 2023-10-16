46bb3a78e11155f0180576ba72e681ede0f6a9f9
Spxncer0 <111756167+Spxncer0@users.noreply.github.com>
2023-10-16 15:37:08 +0100

    Polish strings after user input

--- simpleCalculator.py
+++ simpleCalculator.py
@@ -1,10 +1,10 @@
 # Asking function
 while True:
-    function = input('What function do you want to use?: (Please enter the number)\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\n')
+    function = input('1. Add\n2. Subtract\n3. Multiply\n4. Divide\nWhat function do you want to use? (Please enter the number): ')
     if function == '1' or function == '2' or function == '3' or function == '4':
         break
     else:
-        print('Please enter a valid number!')
+        print('\nPlease enter a valid number!\n')
 
 # Asking numbers
 while True:
@@ -23,13 +23,13 @@ multiply = num1 * num2
 divide = num1 / num2
 
 if function == '1':
-    print(add)
+    print(f'The answer is {add}!')
 
 elif function == '2':
-    print(subtract)
+    print(f'The answer is {subtract}!')
 
 elif function == '3':
-    print(multiply)
+    print(f'The answer is {multiply}!')
 
 elif function == '4':
-    print(divide)
+    print(f'The answer is {divide}!')
