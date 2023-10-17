37327a59a743ab6c6472ef35cc5baa56941c5d46
Spxncer0 <111756167+Spxncer0@users.noreply.github.com>
2023-10-17 10:35:03 +0100

    Add .lower

--- palindromeTester.py
+++ palindromeTester.py
@@ -1,17 +1,17 @@
 # Started 16/10/23
 
 while True:
-    word = input("Type a word/number and see if it palindrome!: ")
+    word = input("Type a word/number and see if it palindrome!: ").lower()
     reversed_word = word[::-1]  # This reverses the user input
     if reversed_word == word:  # This checks if the word is a palindrome
         print("The word/number is a palindrome!")
     if reversed_word != word:
         print("The word/number is NOT a palindrome!")
 
-    again = input("Would you like to input a new word/number? (y/n): ")
+    again = input("Would you like to input a new word/number? (y/n): ").lower()
     while again != "y" and again != "n":
         print("Please enter either y or n...")
-        again = input("Would you like to input a new word/number? (y/n): ")
+        again = input("Would you like to input a new word/number? (y/n): ").lower()
 
     if again == "n":
         print("Have a good day!")
