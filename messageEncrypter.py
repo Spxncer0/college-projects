e5d1010472050f8a6e4fbbe4381ec8067afde358
Spxncer0 <111756167+Spxncer0@users.noreply.github.com>
2023-10-17 10:35:11 +0100

    Add messageEncrypter

--- messageEncrypter.py
+++ messageEncrypter.py
@@ -1,5 +1,22 @@
 # Started 16/10/23
+message = input("Type something for it to be encrypted!:\n")
 
-key = "qwertyuiopasdfghjklzxcvbnm"
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-input_text = input("Please enter a word to be used for the cypher!: ")
+
+def encrypt(message):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    key = "qwertyuiopasdfghjklzxcvbnm"
+
+    result = ""
+    for char in message:
+        if char in alphabet:
+            loc = alphabet.find(char)
+            result += key[loc]
+        else:
+            result += char
+
+    return result
+
+
+result = encrypt(message)
+
+print("encrypted text: " + result)
